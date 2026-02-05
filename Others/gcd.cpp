8e6b566d8fbb7e3e7ce5937b3e4d7af51b324ae4
Adeela Azeez <adeelaazeez9@gmail.com>
2026-02-05 09:49:31 +0530

    added new ques

---
+++ Others/gcd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int rem=INT_MAX;
+    int divident=max(a,b);
+    int divisor=min(a,b);
+    while(rem!=0)
+    {
+        rem=divident%divisor;
+        if(rem>0)
+        {
+            divident=divisor;
+            divisor=rem;
+        }
+    }
+    cout<<divisor<<endl;
+}
