f71d30ba7912e0b3b1fd5709710c280d0664c147
Adeela Azeez <adeelaazeez9@gmail.com>
2026-02-11 10:53:01 +0530

    Added new question

---
+++ Others/countingDivisorsFast.cpp
@@ -0,0 +1,47 @@
+// You are given q queries, each query has a number, calculate no.of factors for each number
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // your code goes here
+    int maxn = 1000000;
+    vector<int> spf(maxn + 1);
+    for (int i = 0; i <= maxn; i++)
+    {
+        spf[i] = i;
+    }
+    for (long long i = 2; i * i <= maxn; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; j <= maxn; j += i)
+            {
+                if (spf[j] == j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> mpp;
+        while (n != 1)
+        {
+            int p = spf[n];
+            mpp[p]++;
+            n = n / spf[n];
+        }
+        int ans = 1;
+        for (auto it : mpp)
+        {
+            ans *= (it.second + 1);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
