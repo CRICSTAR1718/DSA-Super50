ee096d7340a9aa57b01573a5b4b0fc5eda01ea55
Adeela Azeez <adeelaazeez9@gmail.com>
2026-04-01 00:57:45 +0530

    Added new question solution for Codeforces problem "Tram" in C++.

---
+++ Codeforces/tram.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a, b;
+    int current = 0;
+    int max_capacity = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        current -= a; // passengers exit
+        current += b; // passengers enter
+        if (current > max_capacity)
+        {
+            max_capacity = current;
+        }
+    }
+    cout << max_capacity << endl;
+    return 0;
+}
