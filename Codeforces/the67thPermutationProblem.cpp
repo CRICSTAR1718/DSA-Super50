5b2281ee33de8ae0c39f8d103a14d053f2843df9
Adeela Azeez <adeelaazeez9@gmail.com>
2026-04-05 01:21:36 +0530

    Added new files for the 67th Integer and Permutation Problems from Codeforces. These include both the source code (.cpp) and the compiled executables (.exe) for each problem.

---
+++ Codeforces/the67thPermutationProblem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int l = 1, r = 3 * n;
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            ans.push_back(l);     
+            ans.push_back(r - 1); 
+            ans.push_back(r);    
+
+            l++;
+            r -= 2;
+        }
+
+        for (int x : ans)
+            cout << x << " ";
+        cout << "\n";
+    }
+}
