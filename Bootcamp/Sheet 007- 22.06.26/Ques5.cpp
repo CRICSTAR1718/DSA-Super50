121796c1a269ba94ffe89f1c43123228df1a1ad7
Adeela Azeez <adeelaazeez9@gmail.com>
2026-06-23 11:22:23 +0530

    Addded DFS solution for Ques5.cpp in Sheet 007

--- Bootcamp/Sheet 007- 22.06.26/Ques5.cpp
+++ Bootcamp/Sheet 007- 22.06.26/Ques5.cpp
@@ -1,3 +1,5 @@
+// BFS
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -33,3 +35,64 @@ int main()
     }
     return 0;
 }
+
+
+// DFS
+// #include <bits/stdc++.h>
+// using namespace std;
+// bool dfs(int node, vector<vector<int>> &adj, vector<int> &color)
+// {
+//     for (int &it : adj[node])
+//     {
+//         if (color[it] == -1)
+//         {
+//             color[it] = !color[node];
+//             if (!dfs(it, adj, color))
+//             {
+//                 return false;
+//             }
+//         }
+//         else if (color[it] == color[node])
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// int main()
+// {
+//     // your code goes here
+//     int n, m;
+//     cin >> n >> m;
+//     vector<vector<int>> adj(n + 1);
+//     for (int i = 0; i < m; i++)
+//     {
+//         int v, u;
+//         cin >> v >> u;
+//         adj[v].push_back(u);
+//         adj[u].push_back(v);
+//     }
+//     bool flag = true;
+//     vector<int> color(n + 1, -1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (color[i] == -1)
+//         {
+//             color[i] = 0;
+//             if (!dfs(i, adj, color))
+//             {
+//                 flag = false;
+//                 break;
+//             }
+//         }
+//     }
+//     if (flag)
+//     {
+//         cout << "YES" << endl;
+//     }
+//     else
+//     {
+//         cout << "NO" << endl;
+//     }
+//     return 0;
+// }
